ba7d6bf5c09e2c82f83d20789ada86458482d4cc
Vsevolod <11112365@mail.ru>
2019-04-21 22:14:48 +0300

    Initial Commit

---
+++ PointTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PointTask
+//
+//  Created by Всеволод Андрющенко on 21/04/2019.
+//  Copyright © 2019 Всеволод Андрющенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
